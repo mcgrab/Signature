d8440fdbb5ef22069680529371daa05a43acd8f5
mcgrab <mcgrab@yandex.ru>
2021-08-08 16:57:11 +0300

    first implementation

---
+++ FileBlockItem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace Signature
+{
+    public class FileBlockItem
+    {
+        private ManualResetEvent _doneEvent;
+        public byte[] Content { get; set; }
+
+        public int BlockNumber { get; set; }
+
+        public int SizeToHash { get; set; }
+
+        public string Hash { get; private set; }
+
+        public FileBlockItem(ManualResetEvent doneEvent)
+        {
+            _doneEvent = doneEvent;
+        }
+
+        public void CalculateHashMultiBlock(object obj)
+        {
+            using SHA256 sha = SHA256.Create();
+            int offset = 0;
+
+            while (Content.Length - offset >= SizeToHash)
+                offset += sha.TransformBlock(Content, offset, SizeToHash, Content, offset);
+
+            sha.TransformFinalBlock(Content, offset, Content.Length - offset);
+
+            Hash = BytesToStr(sha.Hash);
+            _doneEvent.Set();
+        }
+
+        public void PrintHash()
+        {
+            Console.WriteLine($"block = {BlockNumber + 1}, Hash: {Hash}");
+        }
+
+        private string BytesToStr(byte[] bytes)
+        {
+            StringBuilder str = new StringBuilder();
+
+            for (int i = 0; i < bytes.Length; i++)
+                str.AppendFormat("{0:X2}", bytes[i]);
+
+            return str.ToString();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Signature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var length = new FileInfo(@"C:\\Users\\User\\Downloads\\result_efr.txt").Length;
+            ReadFile(@"C:\\Users\\User\\Downloads\\result_efr.txt", 5000, length);
+        }
+
+        private static void ReadFile(string filePath, int bufferSize, long fileLength)
+        {
+            int MAX_BUFFER = bufferSize;
+            byte[] buffer = new byte[MAX_BUFFER];
+            int bytesRead;
+            var blockNumber = 0;
+            var doneEvents = new ManualResetEvent[(int)Math.Ceiling((double)fileLength / MAX_BUFFER)];
+            var fileBlocks = new FileBlockItem[(int)Math.Ceiling((double)fileLength / MAX_BUFFER)];
+            using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            using (BufferedStream bs = new BufferedStream(fs))
+            {
+                while ((bytesRead = bs.Read(buffer, 0, MAX_BUFFER)) != 0)
+                {
+                    var tempbuffer = new byte[MAX_BUFFER];
+                    Array.Copy(buffer, tempbuffer, MAX_BUFFER); // пока не понял как правильно byteRead или MaxBuffer, влияет на последний блок
+                    // ну и последний блок затирал без копирования, так как ссылка
+                    doneEvents[blockNumber] = new ManualResetEvent(false);
+                    fileBlocks[blockNumber] = new FileBlockItem(doneEvents[blockNumber]) { SizeToHash = bytesRead, BlockNumber = blockNumber, Content = tempbuffer };
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(fileBlocks[blockNumber].CalculateHashMultiBlock));
+                    blockNumber++;
+                }
+            }
+
+            WaitHandle.WaitAll(doneEvents);
+
+            for (int i = 0; i < fileBlocks.Length; i++)
+            {
+                fileBlocks[i].PrintHash();
+            }
+        }
+    }
+}
+
